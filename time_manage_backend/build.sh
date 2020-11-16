aa018a75483d577b5b6e9ebf366a0c4cd819e13d
sheliming <shelimingming@qq.com>
2020-11-16 23:59:36 +0800

    modify build.sh

--- time_manage_backend/build.sh
+++ time_manage_backend/build.sh
@@ -1,2 +1,4 @@
 mvn clean package
 docker build -t time_manage_backend:1.0 .
+sudo docker tag time_manage_backend:1.0 registry.cn-qingdao.aliyuncs.com/time_manage/time_manage_backend:1.0-SNAPSHOT
+sudo docker push registry.cn-qingdao.aliyuncs.com/time_manage/time_manage_backend:1.0-SNAPSHOT
