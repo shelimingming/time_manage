ea6fea3cb3321899aee6ab79001c38842ba15ea3
sheliming <shelimingming@qq.com>
2020-11-29 11:44:55 +0800

    wechat mini program init

--- time_manage_frontend/build.sh
+++ time_manage_frontend/build.sh
@@ -1,2 +1,4 @@
 npm run build
 docker build -t time_manage_frontend:1.0 .
+sudo docker tag time_manage_frontend:1.0 registry.cn-qingdao.aliyuncs.com/time_manage/time_manage_frontend:1.0-SNAPSHOT
+sudo docker push registry.cn-qingdao.aliyuncs.com/time_manage/time_manage_frontend:1.0-SNAPSHOT
--- time_manage_mini/pages/record/record.js
+++ time_manage_mini/pages/record/record.js
@@ -12,7 +12,19 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.request({
+      url: 'https://www.magiccjumboo.top/api/time/version', //仅为示例，并非真实的接口地址
+      data: {
+        x: '',
+        y: ''
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        console.log(res)
+      }
+    })
   },
 
   /**
